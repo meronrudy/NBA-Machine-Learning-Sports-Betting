6df977e479c4178c5f98186eb62e882293b61fc9
rudyrude <34278612+meronrudy@users.noreply.github.com>
2025-01-09 10:47:17 -0500

    Add inference endpoint to main.py
    Add a POST endpoint to receive and process prediction requests.
    
    * **main.py**
      - Import `retreive_model_inference_result` function from `src/Utils/inference.py`.
      
    * **src/Utils/inference.py**
      - Define the `retreive_model_inference_result` function.
      - Implement logging and placeholder model inference logic.
      - Handle exceptions and return a default prediction response.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from src.DataProviders.SbrOddsProvider import SbrOddsProvider
 from src.Predict import NN_Runner, XGBoost_Runner
 from src.Utils.Dictionaries import team_index_current
 from src.Utils.tools import create_todays_games_from_odds, get_json_data, to_data_frame, get_todays_games_json, create_todays_games
+from src.Utils.inference import retreive_model_inference_result
 
 todays_games_url = 'https://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2024/scores/00_todays_scores.json'
 data_url = 'https://stats.nba.com/stats/leaguedashteamstats?' \
